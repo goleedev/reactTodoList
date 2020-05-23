381f498fece089657c9d5ea3d1334d5f7f5de7c8
GO Lee <52603436+goleedev@users.noreply.github.com>
2020-05-23 19:12:22 +0900

    Add delete function

--- reactTodoList/src/components/App.jsx
+++ reactTodoList/src/components/App.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [item, setItem] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    setItem(event.target.value);
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
   }
 
-  function handleClick() {
-    setItems(prevValue => {
-      return [...prevValue, item];
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
     });
-    setItem("");
   }
 
   return (
@@ -20,16 +24,16 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} value={item} type="text" />
-        <button onClick={handleClick}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onAdd={addItem} />
       <div>
         <ul>
-          {items.map(toDoItem => (
-            <li>{toDoItem}</li>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
           ))}
         </ul>
       </div>
