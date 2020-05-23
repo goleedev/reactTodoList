197ff2626ce3dbb3d9f56896e000ab56d9c1ac49
GO Lee <52603436+goleedev@users.noreply.github.com>
2020-05-23 15:56:19 +0900

    Todo list using spread operator

---
+++ reactTodoList/src/components/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    setItem(event.target.value);
+  }
+
+  function handleClick() {
+    setItems(prevValue => {
+      return [...prevValue, item];
+    });
+    setItem("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} value={item} type="text" />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map(toDoItem => (
+            <li>{toDoItem}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
